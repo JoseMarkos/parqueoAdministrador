5ee9cd2f1068722133cc994e2401ea79a4d7b914
Marcos <jkmarcosb@gmail.com>
2019-11-26 15:59:38 -0600

    test add id from cuenta parqueo with the current library

--- Form1.cs
+++ Form1.cs
@@ -23,26 +23,26 @@ namespace ParqueoAdministrator
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Vehicle vehicle = new Vehicle(Vehicle.Vehicletype.Coupe, "Pss", "Marcos", "Holis");
-            Vehicle vehicle2 = new Vehicle(Vehicle.Vehicletype.Coupe, "Pswwwws", "Jose Culajay", "ww");
-            Vehicle vehicle3 = new Vehicle(Vehicle.Vehicletype.Camioneta, "Pss", "Jose Marcos", "aa");
+            CuentaParqueo cuentaParqueo = new CuentaParqueo();
+            CuentaParqueo cuentaParqueo2 = new CuentaParqueo();
+            CuentaParqueo cuentaParqueo3 = new CuentaParqueo();
+
+            Vehicle vehicle = new Vehicle(Vehicle.Vehicletype.Coupe, "Pss", "Marcos", cuentaParqueo.ID, "Holis");
+            Vehicle vehicle2 = new Vehicle(Vehicle.Vehicletype.Coupe, "Pswwwws", "Jose Culajay", cuentaParqueo2.ID, "ww");
+            Vehicle vehicle3 = new Vehicle(Vehicle.Vehicletype.Camioneta, "Pss", "Jose Marcos", cuentaParqueo3.ID, "aa");
 
             listaVehiculos.Add(vehicle);
             listaVehiculos.Add(vehicle2);
             listaVehiculos.Add(vehicle3);
 
-            CuentaParqueo cuentaParqueo = new CuentaParqueo();
-            CuentaParqueo cuentaParqueo1 = new CuentaParqueo();
-            CuentaParqueo cuentaParqueo2 = new CuentaParqueo();
-
             cuentaParqueo.GuardarInformacionPersonal("Marcos", 24);
             cuentaParqueo2.GuardarInformacionPersonal("Jorge", 26);
-            cuentaParqueo1.GuardarInformacionPersonal("Denisse", 23);
+            cuentaParqueo3.GuardarInformacionPersonal("Denisse", 23);
 
             cuentaParqueo.GuardarListaDeCarros(listaVehiculos);
 
             listaCuentaParqueo.Add(cuentaParqueo);
-            listaCuentaParqueo.Add(cuentaParqueo1);
+            listaCuentaParqueo.Add(cuentaParqueo3);
             listaCuentaParqueo.Add(cuentaParqueo2);
 
             BindingSource bindingSource = new BindingSource
